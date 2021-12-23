03ddc1d5ea2bd97cd65a68b9a5aa3722bf0ce1d2
Miti <miti@example.com>
2021-12-22 19:31:16 -0800

    Initial commit.

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+
+void setup() {
+  //Initialize IRQ for RPM measurements
+  //Initialize PWM for fan control
+  //Initialize ADC for RPM control
+  //Initialize OLED
+}
+
+void loop() {
+  //Update RPM measurements
+  //Update PWM output
+  //Read RPM 1-3
+  //Update display
+  //Wait time
+}
